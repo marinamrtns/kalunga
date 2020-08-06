e9878c20cb592e9d5e27909eae032e273a5cb543
Marina Martins <marina.emkt@gmail.com>
2020-08-06 18:50:15 -0300

    add all archives

---
+++ js/javascript.js
@@ -0,0 +1,12 @@
+$(document).ready(function () {
+  $('.banner-rotativo').slick({
+    dots: true,
+    infinite: true,
+    autoplaySpeed: 3000,
+    autoplay: true,
+    accessibility: true,
+    arrows: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+  });
+});
